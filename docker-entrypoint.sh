4e61c9374bd1dd803ec844a3113b8f6401a1d827
marty808 <helge@brust-mail.de>
2022-02-03 20:53:21 +0100

    fix ari user config

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,9 @@ echo "allowed_origins=${ARI_ALLOWED_ORIGINS}" >> /etc/asterisk/ari.conf
 # ARI generate user
 if [ -n "${ARI_USER}" ] && [ -n "${ARI_PASSWORD}" ]; then
    echo "ARI credentials provided."
-   echo -e "\n[${ARI_USER}]\ntype = user\nread_only = no\npassword = ${ARI_PASSWORD}" >> /etc/asterisk/ari.conf
+   if ! grep -q "\[asterisk\]" /etc/asterisk/ari.conf; then
+      echo -e "\n[${ARI_USER}]\ntype = user\nread_only = no\npassword = ${ARI_PASSWORD}" >> /etc/asterisk/ari.conf
+   fi
 else
    echo "ARI credentials not provided"
 fi
